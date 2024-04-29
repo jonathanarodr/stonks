7db6a68085300c3ba10e4c20bb4792dadb7dc79a
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2024-04-29 00:25:29 -0300

    Mapping response service

--- feature/home/src/main/kotlin/br/com/stonks/feature/home/repository/remote/HomeApiService.kt
+++ feature/home/src/main/kotlin/br/com/stonks/feature/home/repository/remote/HomeApiService.kt
@@ -1,12 +1,14 @@
 package br.com.stonks.feature.home.repository.remote
 
+import br.com.stonks.feature.home.repository.remote.response.DailyTransactionResponse
+import br.com.stonks.feature.home.repository.remote.response.WalletResponse
 import retrofit2.http.GET
 
 internal interface HomeApiService {
 
     @GET("/wallet")
-    suspend fun getWallet(): String
+    suspend fun getWallet(): WalletResponse
 
     @GET("/transactions")
-    suspend fun getTransactions(): String
+    suspend fun getTransactions(): DailyTransactionResponse
 }
