6604ba08de3f8b46946cec5a9fd5752d6e3c2d74
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2024-04-28 23:12:07 -0300

    Fixed response type

--- feature/home/src/main/kotlin/br/com/stonks/feature/home/repository/remote/response/TransactionResponse.kt
+++ feature/home/src/main/kotlin/br/com/stonks/feature/home/repository/remote/response/TransactionResponse.kt
@@ -1,11 +1,10 @@
 package br.com.stonks.feature.home.repository.remote.response
 
-import br.com.stonks.feature.home.domain.types.TransactionType
 import com.google.gson.annotations.SerializedName
 
 internal data class TransactionResponse(
     @SerializedName("type")
-    val type: TransactionType,
+    val type: String,
     @SerializedName("description")
     val description: String,
     @SerializedName("value")
