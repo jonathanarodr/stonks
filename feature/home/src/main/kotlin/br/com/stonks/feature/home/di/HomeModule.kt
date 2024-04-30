6c7c1732a7c10316ed4bd79affa6178b96cbbbb5
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2024-04-29 23:57:55 -0300

    Mapping events to fetch mock data

--- feature/home/src/main/kotlin/br/com/stonks/feature/home/di/HomeModule.kt
+++ feature/home/src/main/kotlin/br/com/stonks/feature/home/di/HomeModule.kt
@@ -1,10 +1,16 @@
 package br.com.stonks.feature.home.di
 
+import br.com.stonks.feature.home.domain.mapper.DailyTransactionMapper
+import br.com.stonks.feature.home.domain.mapper.WalletMapper
+import br.com.stonks.feature.home.domain.usecase.DailyTransactionUseCase
+import br.com.stonks.feature.home.domain.usecase.WalletUseCase
 import br.com.stonks.feature.home.repository.HomeRepository
 import br.com.stonks.feature.home.repository.HomeRepositoryImpl
 import br.com.stonks.feature.home.repository.remote.HomeApiService
 import br.com.stonks.feature.home.repository.remote.HomeRemoteDataSource
+import br.com.stonks.feature.home.ui.viewmodel.HomeViewModel
 import br.com.stonks.infrastructure.network.provider.NetworkServiceProvider
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val homeModule = module {
@@ -27,9 +33,38 @@ val homeModule = module {
         )
     }
 
-    factory<HomeRepository> {
+    single<HomeRepository> {
         HomeRepositoryImpl(
             homeRemoteDataSource = get(),
+        )
+    }
+
+    factory {
+        WalletMapper()
+    }
+
+    factory {
+        DailyTransactionMapper()
+    }
+
+    factory {
+        WalletUseCase(
+            homeRepository = get(),
+            walletMapper = get(),
+        )
+    }
+
+    factory {
+        DailyTransactionUseCase(
+            homeRepository = get(),
+            dailyTransactionMapper = get(),
+        )
+    }
+
+    viewModel {
+        HomeViewModel(
+            walletUseCase = get(),
+            dailyTransactionUseCase = get(),
         )
     }
 }
--- feature/home/src/main/kotlin/br/com/stonks/feature/home/ui/viewmodel/HomeViewModel.kt
+++ feature/home/src/main/kotlin/br/com/stonks/feature/home/ui/viewmodel/HomeViewModel.kt
@@ -1,17 +1,49 @@
 package br.com.stonks.feature.home.ui.viewmodel
 
+import androidx.lifecycle.viewModelScope
+import br.com.stonks.common.exception.StonksApiException
 import br.com.stonks.common.states.ViewModelState
+import br.com.stonks.feature.home.domain.usecase.DailyTransactionUseCase
+import br.com.stonks.feature.home.domain.usecase.WalletUseCase
 import br.com.stonks.feature.home.ui.states.HomeUiEvent
 import br.com.stonks.feature.home.ui.states.HomeUiState
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
 
 internal class HomeViewModel(
-
+    private val walletUseCase: WalletUseCase,
+    private val dailyTransactionUseCase: DailyTransactionUseCase,
 ) : ViewModelState<HomeUiState, HomeUiEvent>() {
 
-    override val uiState: MutableStateFlow<HomeUiState> = TODO()
+    override val uiState: MutableStateFlow<HomeUiState> = MutableStateFlow(HomeUiState.Loading)
 
     override fun dispatchUiEvent(uiEvent: HomeUiEvent) {
-        TODO("Not yet implemented")
+        when (uiEvent) {
+            is HomeUiEvent.StartHome -> fetchHomeData()
+        }
+    }
+
+    private fun fetchHomeData() {
+        viewModelScope.launch {
+            fetchWalletData()
+            fetchDailyTransactionData()
+        }
+    }
+
+    private suspend fun fetchWalletData() {
+        walletUseCase().catch {
+            uiState.value = HomeUiState.Error(it as StonksApiException)
+        }.collect {
+            uiState.value = HomeUiState.WalletSuccess(it)
+        }
+    }
+
+    private suspend fun fetchDailyTransactionData() {
+        dailyTransactionUseCase().catch {
+            uiState.value = HomeUiState.Error(it as StonksApiException)
+        }.collect {
+            uiState.value = HomeUiState.DailyTransactionSuccess(it)
+        }
     }
 }
