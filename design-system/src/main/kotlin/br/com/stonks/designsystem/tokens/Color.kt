06e3c234b1694bc2861e9a998ff4dfa71daebee5
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2024-04-30 22:13:51 -0300

    Added design tokens

---
+++ design-system/src/main/kotlin/br/com/stonks/designsystem/tokens/Color.kt
@@ -0,0 +1,27 @@
+package br.com.stonks.designsystem.tokens
+
+import androidx.compose.ui.graphics.Color
+
+object Color {
+
+    /* neutral */
+    val neutralWhite = Color(0xFFFFFFFF)
+    val neutralBack = Color(0xFF000000)
+
+    /* brand */
+    val brandGreenDark = Color(0xFF091B1E)
+    val brandGreenLight = Color(0xFFC3CEC4)
+
+    /* grayscale */
+    val grayscale100 = Color(0xFFDCDCDC)
+    val grayscale200 = Color(0xFFA6A6A6)
+    val grayscale300 = Color(0xFF717171)
+    val grayscale400 = Color(0xFF1A1A1A)
+
+    /* highlight */
+    val highlightPurple = Color(0xFF8F0F4E)
+    val highlightOrange = Color(0xFFEC7000)
+    val highlightRed = Color(0xFFB71C1C)
+    val highlightGreen = Color(0xFF007E49)
+    val highlightBlue = Color(0xFF3896D0)
+}
