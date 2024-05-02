0d2812b4f40bf604de5b6e5c10b51c5ac2b8377b
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2024-05-02 02:43:08 -0300

    Added ds tokens

---
+++ design-system/src/main/kotlin/br/com/stonks/designsystem/tokens/FractionToken.kt
@@ -0,0 +1,15 @@
+package br.com.stonks.designsystem.tokens
+
+object FractionToken {
+    const val level0 = 0.1f
+    const val level1 = 0.1f
+    const val level2 = 0.2f
+    const val level3 = 0.3f
+    const val level4 = 0.4f
+    const val level5 = 0.5f
+    const val level6 = 0.6f
+    const val level7 = 0.7f
+    const val level8 = 0.8f
+    const val level9 = 0.9f
+    const val level10 = 1.0f
+}
